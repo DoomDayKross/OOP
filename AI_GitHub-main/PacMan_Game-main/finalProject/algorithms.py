7bff45cc825e442abd37fe322c38c8e45bc53e47
DoomDayKross <21110777@student.hcmute.edu.vn>
2023-12-04 20:57:22 +0700

    hi

--- AI_GitHub-main/PacMan_Game-main/finalProject/algorithms.py
+++ AI_GitHub-main/PacMan_Game-main/finalProject/algorithms.py
@@ -1,27 +1,34 @@
 from node import *
+from player import *
 import random
-from queue import PriorityQueue
 
 class Algorithms:
     def __init__(self):
+        self.name = ""
+        self.start_node = Node(player(0, 0, 0, 0), [[]], 0, 0, [], 0)
         self.solution = []
-        self.sol_ptr = 0
+        self.solution_ptr = 0
+        self.explore_ptr = 0
         self.depth = 1
         self.nodes = 1
         self.check_list = []
         self.explored = []
-        self.prev_explored_id = []
-
-    def depth_first_search(self, start_node):
-        self.check_list.append(start_node)
+        self.cost = 0
+        # self.goal_position = (-1, -1)
+        # self.f = 0
+        
+    def depth_first_search(self):
+        self.check_list.append(self.start_node)
         while self.check_list:
             node = self.check_list.pop()
-            self.explored.append(node)
-            if not start_node.compare(node):
-                self.solution.append(node.direction)
-                self.depth += 1
-                self.nodes += 1
+            self.nodes += 1
+            if not self.start_node.compare(node) and not self.is_in_explored(node):
+                self.start_node = node.copy()
+                if node.is_goal:
+                    self.solution = node.path
+                    self.depth = node.depth
                 return
+            self.explored.append(node)
             node.expand()
             neighbor_indices = [i for i in range(len(node.nodes))]
             for _ in range(len(node.nodes)):
@@ -29,72 +36,42 @@ class Algorithms:
                 neighbor_indices.remove(i)
                 if not self.is_in_explored(node.nodes[i]):
                     self.check_list.append(node.nodes[i])
-    
-
-
-    def heuristic(self, node, goal_position):
-        pacman_position = node.PacMan.get_position()
-        return abs(pacman_position[0] - goal_position[0]) + abs(pacman_position[1] - goal_position[1])
-
-    def find_nearest_food(self, current_position, food_positions):
-        current_x, current_y = current_position
-
-        nearest_food_position = None
-        nearest_food_distance = float('inf')
-
-        for food_position in food_positions:
-            food_x, food_y = food_position
-            distance = ((food_x - current_x) ** 2 + (food_y - current_y) ** 2) ** 0.5
-
-            if distance < nearest_food_distance:
-                nearest_food_distance = distance
-                nearest_food_position = food_position
-
-        return nearest_food_position
-
-    def a_star_search(self, start_node, food_positions):
-        open_set = PriorityQueue()
-        open_set.put((0, start_node))  # Use a tuple with the priority as the first element
-
-        while not open_set.empty():
-            _, current_node = open_set.get()
-
-            nearest_food_position = self.find_nearest_food(start_node.PacMan.get_position(), food_positions)
-            if current_node.PacMan.get_position() == nearest_food_position:
-                # Goal reached
-                self.solution.append(current_node.direction)
-                self.depth += 1
-                self.nodes += 1
+                    
+    def breadth_first_search(self):
+        self.check_list.insert(0, self.start_node)
+        while self.check_list:
+            node = self.check_list.pop(0)
+            if not self.start_node.compare(node) and not self.is_in_explored(node):
+                self.start_node = node.copy()
+                if node.is_goal:
+                    self.solution = node.path
+                    self.depth = node.depth
                 return
-
-            self.explored.append(current_node)
-            current_node.expand()
-
-            for neighbor in current_node.nodes:
-                if not self.is_in_explored(neighbor):
-                    # Calculate the actual cost for the transition from current_node to neighbor
-                    cost = 1  # Replace this with the actual cost calculation based on your rules
-                    priority = cost + self.heuristic(neighbor, nearest_food_position)
-                    open_set.put((priority, neighbor))
-
-        # No solution found
-        print("No solution found.")
-
-
-    # def breadth_first_search(self, start_node):
-    #     self.check_list.append(start_node)
-    #     while self.check_list:
-    #         node = self.check_list.pop(0)
-    #         self.explored.append(node)
-    #         if not start_node.compare(node):
-    #             self.solution.append(node.direction)
-    #             self.depth += 1
-    #             self.nodes += 1
-    #             return
-    #         node.expand()
-    #         for i in range(len(node.nodes)):
-    #             self.check_list.append(node.nodes[i])
-    #             self.nodes += 1
+            self.explored.append(node)
+            node.expand()
+            for i in range(len(node.nodes)):
+                if not self.is_in_explored(node.nodes[i]):
+                    self.check_list.append(node.nodes[i])
+                    self.nodes += 1
+                    
+    def uniform_cost_search(self):
+        self.check_list.insert(0, self.start_node)
+        while self.check_list:
+            self.sort_check_list("ucs")
+            node = self.check_list.pop(0)
+            if not self.start_node.compare(node) and not self.is_in_explored(node):
+                self.start_node = node.copy()
+                if node.is_goal:
+                    self.solution = node.path
+                    self.depth = node.depth
+                    self.cost = node.cost
+                return
+            self.explored.append(node)
+            node.expand()
+            for i in range(len(node.nodes)):
+                if not self.is_in_explored(node.nodes[i]):
+                    self.check_list.append(node.nodes[i])
+                    self.nodes += 1
                 
     def is_in_explored(self, node):
         for i in range(len(self.explored)):
@@ -102,5 +79,21 @@ class Algorithms:
                 return True
         return False
             
-            
-    
+    def sort_check_list(self, mode):
+        length = len(self.check_list)
+        for i in range(length - 1):
+            if mode == "ucs":
+                min_cost = self.check_list[i].cost
+            if mode == "a_star":
+                min_f = self.check_list[i].cost + self.check_list[i].heuristic_value
+            min_id = i
+            for j in range(1, length):
+                if mode == "ucs" and self.check_list[j].cost < min_cost:
+                    min_cost = self.check_list[j].cost
+                    min_id = j
+                if mode == "a_star" and (self.check_list[j].cost + self.check_list[j].heuristic_value) < min_f:
+                    min_f = (self.check_list[j].cost + self.check_list[j].heuristic_value)
+                    min_id = j
+            if min_id != i:
+                self.check_list[i], self.check_list[min_id] = self.check_list[i].swap(self.check_list[min_id])
+ 
